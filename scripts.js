594009a04a8d779fcdb1d22fdc63caa4d3559d85
[Ethan Jarrell] <[ethan.jarrell@gmail.com]>
2017-06-10 11:29:17 -0400

    update to css and js

--- scripts.js
+++ scripts.js
@@ -3,18 +3,13 @@ var firstNumberArray = [];
 function arrayParsinter(array){
   let firstNumber = 0;
   let secondNumber = 0;
-  for(i=0; i < array.length; i++){
-    let arrayVal = array[i];
-    if(arrayVal != "*" || arrayVal != "/" || arrayVal != "+" || arrayVal != "-")
-  }
 }
-
 for(i=0; i < buttonArray.length; i++){
 
   let buttonId = document.getElementById(buttonArray[i].id);
   let className = buttonId.class;
   console.log(buttonId);
-  if(buttonId === "one" || buttonId === "two" || buttonId === "three" || buttonId === "four" || buttonId === "five" || buttonId === "six" || buttonId === "seven" || buttonId === "eight" || buttonId === "nine" || buttonId === "zero"){
+  if(buttonId === "1" || buttonId === "2" || buttonId === "3" || buttonId === "4" || buttonId === "5" || buttonId === "6" || buttonId === "7" || buttonId === "8" || buttonId === "9" || buttonId === "0"){
     buttonId.addEventListener('click', function storePrint(){
       console.log("button Clicked" + buttonId.id);
       let output = document.getElementById('output');
